dfb37a54c7a3228141afc9272b61070c5d4ff586
acharya100 <drabeshacharya@gmail.com>
2025-08-23 22:07:51 +0545

    modified admin.py

--- blogproject/blog/admin.py
+++ blogproject/blog/admin.py
@@ -2,5 +2,16 @@ from django.contrib import admin
 from .models import Post, Comment
 # Register your models here.
 
-admin.site.register(Post)
-admin.site.register(Comment)
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display=("title","author_name","published","created_at")
+    list_filter=("published","created_at")
+    search_fields = ("title", "content", "author_name")
+    date_hierarchy = "created_at"
+
+
+@admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
+    list_display=("name","post","created_at","active")
+    list_filter=("active","created_at")
+    search_fields=("name","body")
