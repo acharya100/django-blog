022a88d9ece2790a2c44cb2760deff73115e1925
acharya100 <drabeshacharya@gmail.com>
2025-09-03 23:06:35 +0545

    modified serializers

--- blogproject/blog/serializers.py
+++ blogproject/blog/serializers.py
@@ -1,7 +1,25 @@
 from rest_framework import serializers
-from .models import Post
+from .models import Post, Comment
+
+
+class CommentSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Comment
+        fields = ['id', 'name', 'body', 'created_at', 'active']
+
 
 class PostSerializer(serializers.ModelSerializer):
+    comments = CommentSerializer(many=True, read_only=True) 
+
     class Meta:
-        model =Post
-        fields=['id','title','content','published','created_at','updated_at']
+        model = Post
+        fields = [
+            'id',
+            'title',
+            'content',
+            'author_name',
+            'published',
+            'created_at',
+            'updated_at',
+            'comments',  
+        ]
